9b1ab854373744ce639cf66ca25302e88eaf9d11
wujiazhen22 <1599153675@qq.com>
2018-09-29 21:09:49 +0800

    完善dao配置

---
+++ mmorpg_server_dao/src/main/java/com/qworldr/dao/HibernateBaseDao.java
@@ -0,0 +1,10 @@
+package com.qworldr.dao;
+
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
+
+public class HibernateBaseDao<T> extends HibernateDaoSupport implements  IBaseDao{
+
+
+
+
+}
---
+++ mmorpg_server_dao/src/main/java/com/qworldr/dao/IBaseDao.java
@@ -0,0 +1,5 @@
+package com.qworldr.dao;
+
+public interface IBaseDao {
+
+}
