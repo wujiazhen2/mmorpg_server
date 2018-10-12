b4f9893792e6332bf6cd1d0e50f2bb7a31570673
wujiazhen22 <1599153675@qq.com>
2018-10-12 15:24:01 +0800

    资源读取

--- mmorpg_server_dao/src/main/java/com/qworldr/mmorpg/ConfigurationResourceProviderManager.java
+++ mmorpg_server_dao/src/main/java/com/qworldr/mmorpg/ConfigurationResourceProviderManager.java
@@ -1,5 +1,8 @@
 package com.qworldr.mmorpg;
 
+import com.qworldr.mmorpg.provider.EntityProvider;
+import com.qworldr.mmorpg.provider.ResourceProvider;
+import com.qworldr.mmorpg.utils.ReflectUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
@@ -11,10 +14,15 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.Ordered;
 import org.springframework.core.PriorityOrdered;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class ConfigurationResourceProviderManager implements InitializingBean,InstantiationAwareBeanPostProcessor, PriorityOrdered, BeanFactoryAware {
     private String suffix;
     private String path;
     private ConfigurableListableBeanFactory beanFactory;
+    private String scanPackage;
     @Override
     public int getOrder() {
         return Ordered.LOWEST_PRECEDENCE-3 ;
@@ -22,24 +30,36 @@ public class ConfigurationResourceProviderManager implements InitializingBean,In
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        Map<String, ResourceProvider> beansOfType = beanFactory.getBeansOfType(ResourceProvider.class);
+        Set<Class> classes = new HashSet<>();
+        beansOfType.values().forEach(reesourceProvider -> {
+            Class<?> genericType = ReflectUtils.getGenericType(reesourceProvider.getClass());
+            if (genericType != null) {
+                classes.add(genericType);
+            }
+        });
         System.out.println(suffix);
         System.out.println(path);
     }
-
     public String getSuffix() {
         return suffix;
     }
-
     public void setSuffix(String suffix) {
         this.suffix = suffix;
     }
-
     public String getPath() {
         return path;
     }
-
     public void setPath(String path) {
         this.path = path;
+    }
+
+    public String getScanPackage() {
+        return scanPackage;
+    }
+
+    public void setScanPackage(String scanPackage) {
+        this.scanPackage = scanPackage;
     }
 
     @Override
