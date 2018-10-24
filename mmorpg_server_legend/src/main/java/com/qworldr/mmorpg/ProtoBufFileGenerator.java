cdcf82f2b3de585ba0578237d5a4769db92f2794
wujiazhen22 <1599153675@qq.com>
2018-10-24 11:44:57 +0800

    同时根据模板生成客户端使用的协议管理类

--- mmorpg_server_legend/src/main/java/com/qworldr/mmorpg/ProtoBufFileGenerator.java
+++ mmorpg_server_legend/src/main/java/com/qworldr/mmorpg/ProtoBufFileGenerator.java
@@ -2,7 +2,14 @@ package com.qworldr.mmorpg;
 
 import com.baidu.bjf.remoting.protobuf.ProtobufIDLGenerator;
 import com.qworldr.mmorpg.annotation.Protocal;
+import com.qworldr.mmorpg.common.anno.Desc;
+import com.qworldr.mmorpg.common.protocal.ProtocalId;
 import org.apache.commons.io.FileUtils;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.context.Context;
+import org.hibernate.annotations.common.reflection.ReflectionUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
@@ -11,12 +18,10 @@ import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
 import org.springframework.core.type.classreading.MetadataReader;
 import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.util.ReflectionUtils;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 /**
  * @Author wujiazhen
@@ -25,8 +30,10 @@ public class ProtoBufFileGenerator {
     public static final String PATH = "proto";
     private static  final Logger logger = LoggerFactory.getLogger(ProtobufIDLGenerator.class);
     public static final String COM_QWORLDR_MMORPG_CLASS = "classpath:com/qworldr/mmorpg/**/*.class";
+    private static Map<Short,String> descs= new HashMap<>();
 
     public  static  void main(String[] args) throws IOException, ClassNotFoundException {
+        loadDesc();
         ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
         Resource[] resources = resourcePatternResolver.getResources(COM_QWORLDR_MMORPG_CLASS);
         MetadataReaderFactory metadataReaderFactory = new CachingMetadataReaderFactory(resourcePatternResolver);
@@ -36,10 +43,13 @@ public class ProtoBufFileGenerator {
         String idl;
         Set<Class<?>> types = new HashSet<>();
         Set<Class<?>> sets = new HashSet<>();
+        Map<Short,String> packets= new HashMap<>();
         for (Resource resource : resources) {
             metadataReader = metadataReaderFactory.getMetadataReader(resource);
             if(metadataReader.getAnnotationMetadata().hasAnnotation(Protocal.class.getName())){
                 cls = Class.forName(metadataReader.getClassMetadata().getClassName());
+                Protocal annotation = cls.getAnnotation(Protocal.class);
+                packets.put(annotation.value(),cls.getSimpleName());
                 idl = ProtobufIDLGenerator.getIDL(cls,types,sets,true);
                 file =new File(PATH +File.separator+cls.getSimpleName()+".proto");
                 if(!file.getParentFile().exists()){
@@ -53,5 +63,36 @@ public class ProtoBufFileGenerator {
                 logger.debug(String.format("生成protobuf文件：%s",file.getAbsoluteFile()));
             }
         }
+        //输出模板
+        generateProtoJS(packets);
+        return;
+    }
+
+    public static void loadDesc() {
+        //加载描述
+        ReflectionUtils.doWithFields(ProtocalId.class, field -> {
+            Desc annotation = field.getAnnotation(Desc.class);
+            field.setAccessible(true);
+            descs.put((Short) field.get(null),annotation.value());
+        });
+    }
+
+    public static void generateProtoJS(Map<Short, String> packets) throws IOException {
+        Properties p = new Properties();
+        p.put("file.resource.loader.class",
+                "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        Velocity.init(p);
+        Template template = Velocity.getTemplate("vm/protocalManager.vm","UTF-8");
+        Context context = new VelocityContext();
+        context.put("PACKETS",packets);
+        context.put("DESC",descs);
+        File fileJS=new File(PATH+File.separator+"protocalManager.js");
+        FileOutputStream outStream = new FileOutputStream(fileJS);
+        OutputStreamWriter writer =  new OutputStreamWriter(outStream,"UTF-8");
+        BufferedWriter sw = new BufferedWriter(writer);
+        template.merge(context,sw);//根据模板文件以及输入的contezt生成代码
+        sw.flush();
+        sw.close();
+        outStream.close();
     }
 }
