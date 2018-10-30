0922d105c937ef5bf00346a8408189dac97ae83b
wujiazhen22 <1599153675@qq.com>
2018-10-30 15:39:20 +0800

    测试

--- mmorpg_server_legend/src/test/java/com/qworldr/mmorpg/SnowflakeIdentifyGeneratorStrategyTest.java
+++ mmorpg_server_legend/src/test/java/com/qworldr/mmorpg/SnowflakeIdentifyGeneratorStrategyTest.java
@@ -1,9 +1,11 @@
 package com.qworldr.mmorpg;
 
 import com.qworldr.mmorpg.common.identify.SnowflakeIdentifyGeneratorStrategy;
+import org.apache.commons.collections.set.SynchronizedSet;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
@@ -13,27 +15,32 @@ import java.util.concurrent.Executors;
 /**
  * @Author wujiazhen
  */
-public class SnowflakeIdentifyGeneratorStrategyTest extends SpringTest {
+public class SnowflakeIdentifyGeneratorStrategyTest  {
 
-    @Autowired
-    private SnowflakeIdentifyGeneratorStrategy snowflakeIdentifyGeneratorStrategy;
 
     @Test
     public void test() throws InterruptedException {
+        SnowflakeIdentifyGeneratorStrategy snowflakeIdentifyGeneratorStrategy=new SnowflakeIdentifyGeneratorStrategy();
+        snowflakeIdentifyGeneratorStrategy.setServerId(1023);
         ExecutorService executorService = Executors.newFixedThreadPool(10);
-        Set<Long> longs = new HashSet<>();
-        CountDownLatch countDownLatch =new CountDownLatch(10000);
-        for(int i=0;i<100000;i++){
+        Set<Long> longs = Collections.synchronizedSet(new HashSet<>());
+        CountDownLatch countDownLatch =new CountDownLatch(30000);
 
+        long start=System.currentTimeMillis();
+        for(int i=0;i<30000;i++){
             executorService.submit(()->{
-
                 Long x = snowflakeIdentifyGeneratorStrategy.generatorKey();
+
                 if(longs.contains(x)){
-                    System.out.println("相同了------------------------------------------------------");
+                    System.out.println("相同了------------------------------------------"+x);
+                }else {
+                    longs.add(x);
                 }
                 countDownLatch.countDown();
             });
         }
         countDownLatch.await();
+        long end=System.currentTimeMillis();
+        System.out.println((end-start));
     }
 }
--- mmorpg_server_legend/src/test/java/com/qworldr/mmorpg/SpringTest.java
+++ mmorpg_server_legend/src/test/java/com/qworldr/mmorpg/SpringTest.java
@@ -1,5 +1,6 @@
 package com.qworldr.mmorpg;
 
+import com.qworldr.mmorpg.common.identify.SnowflakeIdentifyGeneratorStrategy;
 import com.qworldr.mmorpg.logic.player.entity.PlayerEntity;
 import com.qworldr.mmorpg.provider.EntityProvider;
 import org.hibernate.SessionFactory;
@@ -8,6 +9,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
