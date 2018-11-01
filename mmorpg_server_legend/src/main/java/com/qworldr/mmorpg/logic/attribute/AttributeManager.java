fca83b6a2f98a4ebb57774aabc6acf5caab567b2
wujiazhen22 <1599153675@qq.com>
2018-11-01 21:12:35 +0800

    属性系统ing

--- mmorpg_server_legend/src/main/java/com/qworldr/mmorpg/logic/attribute/AttributeManager.java
+++ mmorpg_server_legend/src/main/java/com/qworldr/mmorpg/logic/attribute/AttributeManager.java
@@ -5,6 +5,11 @@ import com.qworldr.mmorpg.logic.attribute.enu.AttributeType;
 
 import java.util.Map;
 
+/**
+ * @Author wujiazhen
+ * 影响属性的因素有 属性点，装备，称号。。。多个模块。以属性树的形式记录属性的起源。
+ *
+ */
 public class AttributeManager {
     public void setAttr(AttributeSourceType sourceType, Map<AttributeType, Integer> attrs) {
     }
